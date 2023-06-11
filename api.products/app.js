18429f4d35509368ccbaad8ffe11f1aa21cb89b4
KowshikI87 <kowshikislam8@gmail.com>
2023-06-11 19:59:16 -0400

    remove unnecessary debugging code from backend

--- api.products/app.js
+++ api.products/app.js
@@ -6,31 +6,21 @@ const catchError = require("./lib/catch-error");
 const app = express();
 
 //logging requests
-// app.use(morgan("combined"));
-// app.use(morgan(':method :url :status :response-time ms - :res[content-length] :req[header]'));
-
-// app.use((req, res) => {
-//   res.status(409).send(`original URL is ${req.originalUrl}`);
-// });
-
+app.use(morgan("common"));
 
 //main route: returns active product list
-app.get("/api/products/", catchError(async (req, res) => {
+app.get("/api/products/", catchError(async (_req, res) => {
   let products = await getProducts();
   let activeProducts = products.filter(product => {
     return product.ProductStatus === "Active";
   });
   res.json(activeProducts);
-  //res.send("Products route");
 }));
 
-app.get('/test', (req, res) => res.send("Test route"));
-
 //Error handler
-app.use((err, req, res, _next) => {
+app.use((err, _req, res, _next) => {
   console.log(err);
-  //res.status(404).send(err.message);
-  res.status(404).send(req.originalUrl);
+  res.status(404).send(err.message);
 });
 
 module.exports = app;
